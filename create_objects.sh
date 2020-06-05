0c903ae8e3819645401f73d27bd722c117025ac0
amoya87 <17645028+amoya87@users.noreply.github.com>
2020-06-05 11:11:07 -0400

    -Import utilities with source -Added exit code -Formated using google Shell Style Guide https://google.github.io/styleguide/shellguide.html -Removed search in groups_file -Extract "MJD-OBS" and "EXPTIME" from diffhead using fitshdr command -Removed .objects temporal file -created .objemjd  temporal file -Remove times.sh script -Call candids.sh script with exposures param

--- create_objects.sh
+++ create_objects.sh
@@ -4,82 +4,75 @@
 # candidate transient objects as an ordered list of ATLAS detections grouped by DEC & RA within a given tolerance
 
 # Import utils
-. ./utils.sh --source-only
+source utils.sh
 
 if [ "$#" -ne 3 ]; then
     echo " "; echo "Usage: $0 [tessellation name] [output group file from imggrps.sh] tolerance"
     echo "Script asks for an ATLAS style tessellation ID, the imggrps.sh output with the exposures associated to each tessellation,"
     echo "and the tolerance in arcseconds to match detections in different *.ddc files and declare they are the same object." ; echo " "
-    exit
+    exit 1
 fi
 
 startime=`date +%s.%N` ; cmd=$0 # <--- define initial parameters for function elapsed
 # Call it with: echo $(elapsed) # Report elapsed time & calling command
 
-tesse=$1 ; groups_file=$2 # e.g.: tesse: SV341N74
-echo "Looking for tessellation $tesse"
-telnite="${groups_file:0:8}" # todo: change this, may be too custom, better to parse '_'. e.g.: 02a58884
+tesse=$1 ; exposures=$2 # e.g.: tesse: SV341N74
+telnite="${exposures:0:8}" # todo: change this, may be too custom, better to parse '_'. e.g.: 02a58884
 tessetelnite="${tesse}_${telnite}" # e.g.: SV341N74_02a58884
 
 tol=$(echo "$3" | awk '{printf"%s\n",($1/3600.)""}') # ttt is the tolerance in degrees
 
-# Check whether the group file is there and can be read
-validate_file ${groups_file} "groups"
+# Exposures as array
+istesse=(${exposures})
 
-# If the file exists and can be read check that the tessellation requested is in the file
-# Append tessellation + exposures
-istesse+=($(grep ${tesse} ${groups_file})) # echo "${istesse[*]}"
+if [ -d ${tessetelnite} ]; then      # If a directory named tessetelnite exists remove it and make it clean again
+  /bin/rm -r "${tessetelnite}"
+  mkdir "${tessetelnite}"
+else
+  mkdir "${tessetelnite}"
+fi
+# Prepares a file with the name of the tesselation and the associated exposures
+#echo "${tesse} ${istesse[@]}" > "${tessetelnite}/${tessetelnite}.exposures" # e.g.: SV341N74_02a58884
+awkword=''
+for (( i = 0 ; i < (${#istesse[@]}); i++ )); do # do this for every argument but the tessellation
+  exp="${istesse[i]}"
+  ddcfile="/atlas/diff/${exp:0:3}/${exp:3:5}/${exp}.ddc" #; echo "${ddcfile}" # second argument is how many characters to take
+  # Wait for the creation of the ddc file
+  wait_for_file ${ddcfile} "2"
+  
+  # If the file exists and is readable
+  validate_file ${ddcfile}  
+  awkword=${awkword}" ${ddcfile}" # awkword is a string with the ddc file paths/names separated by spaces
+  
+  diffimg="/atlas/diff/${exp:0:3}/${exp:3:5}/${exp}.diff.fz"       #; echo "${diffimg}"
+  validate_file ${diffimg}  
+  
+  diffhead=$(/atlas/bin/fitshdr ${diffimg})		# Store the header of the diff images for further use
+  #diffheadfile="${exp}.diff.head"
+  #echo $diffhead >  "${tessetelnite}/${diffheadfile}"
+  expstart=$(echo "${diffhead}" | grep "MJD-OBS" | awk '{print $3}') #; echo "${expstart}"
+  exptime=$(echo "${diffhead}" | grep "EXPTIME" | awk '{print $3}')  #; echo "${exptime}"
+  timeobs[i]=$(echo $expstart $exptime | awk '{printf"%12.6f", ($1+($2/86400)/2)}') #; echo "${timeobs[i]} $i"
+done
 
-if [ "${#istesse[@]}" -eq 0 ] ; then
-  echo "Tessellation ${tesse} is not in ${groups_file}." ; echo " "
-  exit
-else                                    # else everything is ready to look for the associated exposures and their products
-  if [ -d ${tessetelnite} ] ; then      # If a directory named tessetelnite exists remove it and make it clean again
-     /bin/rm -r "${tessetelnite}"
-     mkdir "${tessetelnite}"
-  else
-    mkdir "${tessetelnite}"
-  fi
-  # Prepares a file with the name of the tesselation and the associated exposures
-  echo "${istesse[@]}" > "${tessetelnite}/${tessetelnite}.exposures" # e.g.: SV341N74_02a58884
-  awkword=''
-  for (( i = 1 ; i <= (${#istesse[@]}-1); i++ )) ; do # do this for every argument but the tessellation
-    exp="${istesse[i]}"
-    ddcfile="/atlas/diff/${exp:0:3}/${exp:3:5}/${exp}.ddc" #; echo "${ddcfile}" # second argument is how many characters to take
-    # Wait for the creation of the ddc file
-    wait_for_file ${ddcfile} "2"
-    # If the file exists and is readable
-    if [ -f ${ddcfile} ] && [ -r ${ddcfile} ] ; then
-      awkword=${awkword}" ${ddcfile}" # awkword is a string with the ddc file paths/names separated by spaces
-    else
-      validate_file ${ddcfile} "ddc" # Todo: unefficient
-    fi
-    diffimg="/atlas/diff/${exp:0:3}/${exp:3:5}/${exp}.diff.fz"       #; echo "${diffimg}"
-    if [ -f ${diffimg} ] && [ -r ${diffimg} ] ; then
-      diffhead="${exp}.diff.head"                                    #; echo ${diffhead}
-      /atlas/bin/fitshdr ${diffimg} > "${tessetelnite}/${diffhead}"    # Store the header of the diff images for further use
-    else
-      validate_file ${diffimg} "diffimg"
-    fi
-  done
-  nddc=$(echo ${awkword} | awk '{print NF}')
-  if [ "${nddc}" -le 1 ] ; then
-    echo "Only one .ddc file to look for object candidates?"
-    exit
-  else
 # Following line is key: jointly scans all the .ddc files of the tessellation taking only detections that a priori have more than 1% chance of being interesting to Alerce. Keeps the .ddc record, but adds
 # the exposure name and order of appearance in the .ddc file for later use in the candid name. Sorts the detections using declination.
-    awk '$1~"#"{n0=NR};$1!~"#"&&$21>=0&&(1000-($14+$15+$16+$17+$18+$19+$20))>=10{print $0,substr(FILENAME,23,14),NR-n0}' ${awkword} | sort -n -k2 > "${tessetelnite}/${tesse}_candids.tmp"
+awk '$1~"#"{n0=NR};$1!~"#"&&$21>=0&&(1000-($14+$15+$16+$17+$18+$19+$20))>=10{print $0,substr(FILENAME,23,14),NR-n0}' ${awkword} > "${tessetelnite}/${tesse}_candids.tmp"
 # Following line is key as well: Process the file with the filtered and sorted detections and finds those that coincide within ${tol}. Assign an object number and gives a unique name to each group
 # Uses Tonry's column merge program "cm" in friends of friends mode.
-    /atlas/bin/cm 2,1 "${tessetelnite}/${tesse}_candids.tmp" -tol ${tol},d -grp -grporder | awk -v obj=${tessetelnite} '{printf"%s %17s_%04g\n",$0,obj,$1}' > "${tessetelnite}/${tessetelnite}.objects"
-  fi
-fi
+/atlas/bin/cm 2,1 "${tessetelnite}/${tesse}_candids.tmp" -tol ${tol},d -grp -grporder | awk -v obj="${tessetelnite}" -v timeobs="${timeobs[*]}" -v exposures="${istesse[*]}" ' \
+BEGIN { split(timeobs,times," "); \
+        split(exposures,exps," "); \
+		for(i in exps){ \
+		  values[exps[i]]=times[i] \
+		} \
+ } \
+ {printf" %9.5f %9.5f %7.3f %6.3f %8.2f %8.2f %6.2f %6.2f %6.2f %2d %6.2f %3d %3d %3d %3d %3d %3d %3d %3d %3d %2d %9.1f %6.1f %s_%06g %17s_%04g %12.6f\n", \
+          $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,obj,$1,values[$25]}' > "${tessetelnite}/${tessetelnite}.objemjd"
+
 /bin/rm "${tessetelnite}/${tesse}_candids.tmp" # Delete temporary file
 # here tessetelnite is ready
-echo "Calling times for ${tesse}"
-./times.sh ${tessetelnite}
 echo "Calling candids for ${tesse}"
-./candids.sh ${tessetelnite}
+./candids.sh ${tessetelnite} "${exposures}"
 
 echo $(elapsed) "${tessetelnite}" # Report elapsed time
