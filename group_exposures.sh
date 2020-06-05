cf4e744eac4ed81447be98a7c7cb5c59ef245e8a
amoya87 <17645028+amoya87@users.noreply.github.com>
2020-06-05 10:40:14 -0400

    -Import utilities with source -Added exit code -Formated using google Shell Style Guide https://google.github.io/styleguide/shellguide.html -Removed some unnecessary validations -Added local by default to all variables inside functions -Removed .groups temporal file -Call create_objects.sh script with exposures param

--- group_exposures.sh
+++ group_exposures.sh
@@ -3,89 +3,87 @@
 # For a given telescope and camera and a given night, group the exposures based on the corresponding tessellation.
 
 # Import utilities
-. ./utils.sh --source-only
+source utils.sh
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 telescope+camera_ID night_number"
     echo "Script needs a telescope + camera id (e.g. 02a) and an ATLAS style night number (e.g. 58XXX)."
-    exit
+    exit 1
 fi
 
-telescope=$1 ; nite=$2
-
 ## Global variables ##
+telescope=$1; nite=$2
 # Path to log file
 log_path="/atlas/red"
 log_file="${log_path}/${telescope}/${nite}/${telescope}${nite}.log"
 # Associative array to hold the exposure's groups
 declare -A groups
 
-# Validate user input
+#######################################
+# Validate user input.
+# Globals:
+#   telescope
+#   nite
+#   log_path
+#   log_file
+# Errors:
+#   exit 1 if the inputs is invalid
+#######################################
 validate_input () {
-  # Check the first and last nights available for the requested telescope
-  nites=$(ls -1 ${log_path}/${telescope}/ | wc | awk '{print $1}')
-  nnf=$(ls ${log_path}/${telescope}/ | awk 'NR==1{print substr($0,1,5)}') # First nite in ATLAS database
-  nnl=$(ls ${log_path}/${telescope}/ | awk -v fin=${nites} 'NR==fin{print substr($0,1,5)}') # Last nite in ATLAS database
-
-  if [ ${telescope} != "01a" ] && [ ${telescope} != "02a" ] ;  then
-    echo "Telescope has to be either 01a or 02a."
-    exit
-  fi
-
-  if [ ${nite} -gt ${nnl} ] ; then
-    echo "Last nite of telescope ${telescope} in reduced directory is ${nnl}"
-    exit
-  fi
-
-  if [ ${nite} -lt ${nnf} ] ; then
-    echo "First nite of telescope ${telescope} in reduced directory is ${nnf}"
-    exit
+  if [ ${telescope} != "01a" ] && [ ${telescope} != "02a" ]; then
+    err "Telescope has to be either 01a or 02a."
+    exit 1
   fi
 
   # If nite and telescope are within expectations check whether the night is there and can be executed.
-  if [ ! -d "${log_path}/${telescope}/${nite}/" ] ; then
-    echo "Directory ${log_path}/${telescope}/ does not exist."
-    exit
-  fi
-
-  if [ ! -x "${log_path}/${telescope}/${nite}/" ] ; then
-    echo "Directory ${log_path}/${telescope}/ is not executable."
-    exit
-  fi
+  validate_directory ${log_path}/${telescope}/${nite}
 
   # If the nite's directory is there and can be opened, check whether a log file was produced and can be read
-  validate_file $log_file "logfile"
+  validate_file $log_file
 }
-
-# Get the pointing and exposure data
-# Parameter: $1, current line
+ 
+#######################################
+# Get the pointing and exposure data.
+# Globals:
+#   log_file
+# Arguments:
+#   Log file line
+# Outputs:
+#   Writes observation and tessellation to stdout
+#######################################
 grab_data () {
   local current_line=$1
-  exposure_pointing="$( sed "${current_line}q;d" ${log_file} | awk '{print $1, $17}' )"
+  local exposure_pointing="$( sed "${current_line}q;d" ${log_file} | awk '{print $1, $17}' )"
   echo $exposure_pointing
 }
 
-# Function to process the data gotten from the grab function
+#######################################
+# Function to process the data gotten from the grab function.
+# Globals:
+#   groups
+#   telescope
+#   nite
+# Arguments:
+#   Observation and tessellation separete by space
+#######################################
 process_data () {
-  data=$1
+  local data=$1
   set -- $data
-  exposure=$1
-  tessellation=$2
+  local exposure=$1
+  local tessellation=$2
   # Grab value and put it on the map
-  current_array=(${groups[$tessellation]})
+  local current_array=(${groups[$tessellation]})
   # Check whether the key tessellation is already in the map, if not, add it and then add the exposure
   # Ignore preflight and twiflat values
-  if [ "$tessellation" != "preflight" ] && [ "$tessellation" != "twiflat" ]
-  then
+  if [ "$tessellation" != "preflight" ] && [ "$tessellation" != "twiflat" ]; then
     current_array+=("$exposure")
     groups[$tessellation]="${current_array[@]}"
     # TODO: reject if exposure already in the list
-    if [ ${#current_array[@]} -eq 4 ]
-    then
-      echo "$tessellation ${groups[$tessellation]}" >> "${telescope}${nite}_img.groups"
+    if [ ${#current_array[@]} -eq 4 ]; then
+#      echo "$tessellation ${groups[$tessellation]}" >> "${telescope}${nite}_img.groups"
       # Call create_objects, next step in the pipeline
-      tolerance="1.9"
-      ./create_objects.sh $tessellation "${telescope}${nite}_img.groups" $tolerance &
+      local tolerance="1.9"
+      ./create_objects.sh $tessellation "${groups[$tessellation]}" $tolerance &
     fi
   fi
 }
