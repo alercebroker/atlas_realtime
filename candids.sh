36fc699fd8acf25865346c22b40ca96b3d7cd56e
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2023-12-05 19:01:59 -0300

    refactor: Save tessetelnite data in data folder

--- candids.sh
+++ candids.sh
@@ -7,11 +7,11 @@
 source utils.sh
 
 if [ "$#" -ne 2 ]; then
-    echo " "; echo "Usage: $0 tessellation_telnite exposures"
-    echo "Where tessellation_telnite is the directory created by create_objects.sh, and"
-    echo "exposures is the list of exposures of the current tessellation"
-    echo " "
-    exit
+  echo " "; echo "Usage: $0 tessellation_telnite exposures"
+  echo "Where tessellation_telnite is the directory created by create_objects.sh, and"
+  echo "exposures is the list of exposures of the current tessellation"
+  echo " "
+  exit
 fi
 
 startime=`date +%s.%N`     ; cmd=$0                    # <--- define initial parameters for function elapsed
@@ -19,7 +19,7 @@ startime=`date +%s.%N`     ; cmd=$0                    # <--- define initial par
 tessetelnite=$1
 exposures=$2
 
-objname="${tessetelnite}/${tessetelnite}.objemjd" # ; echo "${tessetelnite} ${objname}"
+objname="data/${tessetelnite}/${tessetelnite}.objemjd" # ; echo "${tessetelnite} ${objname}"
 validate_file  "${objname}"
 
 expname=(${exposures})                          # ; echo "${expname[@]}"
@@ -32,14 +32,14 @@ let "nexpo = ${#expname[@]}"                      # this is the number of exposu
 
 #awk -v ll=${nlines} 'NR==1{o=$25;a=$1;d=$2;t=$26;n=1;l=$25};NR>1&&NR<=ll&&$25==o{a=a+$1;d=d+$2;t=t+$26;n=n+1;l=$25};\
 #  NR>1&&NR<=ll&&$25!=o{printf"%s %9.5f %9.5f %12.6f %d\n",l,a/n,d/n,t/n,n;a=$1;d=$2;t=$26;n=1;o=$25};\
-#  END{printf"%s %9.5f %9.5f %12.6f %d\n",l,a/n,d/n,t/n,n}' "${objname}" > "${tessetelnite}/${tessetelnite}_objects.data"
+#  END{printf"%s %9.5f %9.5f %12.6f %d\n",l,a/n,d/n,t/n,n}' "${objname}" > "data/${tessetelnite}/${tessetelnite}_objects.data"
 
 while read -r candinfo; do
   # expnome is a string read from objname, expname is an array read from expinfo and kept in memory (refer to the same objects but are different variables here)
   expnomecandlab=$(echo "${candinfo}" | awk '{print $24}')                          #; echo "${expnome}_${candlab}"
   expnome=$(echo "${expnomecandlab}" | awk -F"_" '{print $1}')                      #; echo "${expnome}"
   candfile="${expnomecandlab}.info"    # ; echo $candfile
-  printf "%s\n" "${candinfo}" > "${tessetelnite}/${candfile}"                      # output 1-line file with candidate info
+  printf "%s\n" "${candinfo}" > "data/${tessetelnite}/${candfile}"                      # output 1-line file with candidate info
 
   # prepare the stamp commands for monsta. All of them mixed together in this loop to split and activate exposure by exposure in next loop
   # TODO validation "if ($5 < 10544)" to avoid monsta errors when stamp overflows xedge. Remove when monsta solve it.
@@ -50,9 +50,9 @@ while read -r candinfo; do
   istamp="${expnomecandlab}_istamp.fits"    #; echo $istamp
   #tstamp="${expnomecandlab}_tstamp.fits"    #; echo $tstamp
   
-  dmonstastamps="${tessetelnite}/${expnome}_d_stamps.monsta"                                   # name of the file for the monsta commands for difference image
-  imonstastamps="${tessetelnite}/${expnome}_i_stamps.monsta"                                   # name of the file for the monsta commands for science image
-  #tmonstastamps="${tessetelnite}/${expnome}_t_stamps.monsta"                                   # name of the file for the monsta commands for template image
+  dmonstastamps="data/${tessetelnite}/${expnome}_d_stamps.monsta"                                   # name of the file for the monsta commands for difference image
+  imonstastamps="data/${tessetelnite}/${expnome}_i_stamps.monsta"                                   # name of the file for the monsta commands for science image
+  #tmonstastamps="data/${tessetelnite}/${expnome}_t_stamps.monsta"                                   # name of the file for the monsta commands for template image
   if [[ -n $xy ]]; then
     echo "${xy} ${tessetelnite}/${dstamp}" >> "${dmonstastamps}"                           # command line of the form x y stamp_file_name for a difference stamp
     echo "${xy} ${tessetelnite}/${istamp}" >> "${imonstastamps}"                           # command line of the form x y stamp_file_name for an image stamp
@@ -62,41 +62,41 @@ done < ${objname}
 
 for (( i = 0 ; i < ${nexpo}; i++ )); do
 
-  nstamps=$(wc -l "${tessetelnite}/${expname[i]}_d_stamps.monsta" | awk '{print $1}')           #   ; echo "${nstamps}"               # nstamps is the same for diff, img, and template
+  nstamps=$(wc -l "data/${tessetelnite}/${expname[i]}_d_stamps.monsta" | awk '{print $1}')           #   ; echo "${nstamps}"               # nstamps is the same for diff, img, and template
 
   # prepare monsta commands for the difference image of this exposure
   diffimg="/data/atlas-local/diff/${expname[i]:0:3}/${expname[i]:3:5}/${expname[i]}.diff.fz"                # ; echo "${diffimg}"
   validate_file "${diffimg}"
-  dmonstastamps="${tessetelnite}/${expname[i]}_d_stamps.monsta"                                  # name of the file for the monsta commands for difference image
+  dmonstastamps="data/${tessetelnite}/${expname[i]}_d_stamps.monsta"                                  # name of the file for the monsta commands for difference image
   sed -i "1s/^/${nstamps}\n/" ${dmonstastamps}                                                # first line in the file of commands for monsta
   monsta /atlas/src/trunk/red/subarrays.pro ${diffimg} ${dmonstastamps} $stamp_size
-  find "${tessetelnite}" -name *.fits -print0 | parallel  --will-cite -0 gzip
+  find "data/${tessetelnite}" -name *.fits -print0 | parallel  --will-cite -0 gzip
   
   # prepare monsta commands for the science image of this exposure
   scieimg="/data/atlas-local/red/${expname[i]:0:3}/${expname[i]:3:5}/${expname[i]}.fits.fz"             #  ; echo "$scieimg"
   validate_file "${scieimg}"
-  imonstastamps="${tessetelnite}/${expname[i]}_i_stamps.monsta"                                   # name of the file for the monsta commands for science image
+  imonstastamps="data/${tessetelnite}/${expname[i]}_i_stamps.monsta"                                   # name of the file for the monsta commands for science image
   sed -i "1s/^/${nstamps}\n/" ${imonstastamps}                                                # first line in the file of commands for monsta
   monsta /atlas/src/trunk/red/subarrays.pro ${scieimg} ${imonstastamps} $stamp_size
-  find "${tessetelnite}" -name *.fits -print0 | parallel  --will-cite -0 gzip
+  find "data/${tessetelnite}" -name *.fits -print0 | parallel  --will-cite -0 gzip
 
   ### prepare monsta commands for the template image of this exposure
   ##  tempimg="/data/atlas-local/red/${expname[i]:0:3}/${expname[i]:3:5}/${expname[i]}.fits.fz"             #  ; echo "$tempimg"           # same as science image for the time being!!!
   ##  validate_file "${tempimg}"
-  ##  tmonstastamps="${tessetelnite}/${expname[i]}_t_stamps.monsta"                                   # name of the file for the monsta commands for template image
+  ##  tmonstastamps="data/${tessetelnite}/${expname[i]}_t_stamps.monsta"                                   # name of the file for the monsta commands for template image
   ##  sed -i "1s/^/${nstamps}\n/"  ${tmonstastamps}                                                # first line in the file of commands for monsta
   ##  monsta /atlas/src/trunk/red/subarrays.pro ${tempimg} ${tmonstastamps} $stamp_size
   
 done
 
 # clean up the monsta command files
-/bin/rm "${tessetelnite}"/*.monsta
+/bin/rm "data/${tessetelnite}"/*.monsta
 
 out $(elapsed) "${tessetelnite}"                         # Report elapsed time
 
 # Create the directory where to put the avro files
-if [ ! -d "${tessetelnite}/avro" ]
+if [ ! -d "data/${tessetelnite}/avro" ]
 then
-    mkdir "${tessetelnite}/avro"
+  mkdir "data/${tessetelnite}/avro"
 fi
-go run config.go create_records.go confluent_producer.go  generate_alerts.go "${tessetelnite}"
+go run config.go create_records.go confluent_producer.go  generate_alerts.go "data/${tessetelnite}"
--- create_objects.sh
+++ create_objects.sh
@@ -16,6 +16,11 @@ fi
 startime=`date +%s.%N` ; cmd=$0 # <--- define initial parameters for function elapsed
 # Call it with: echo $(elapsed) # Report elapsed time & calling command
 
+if [ ! -d "data" ]
+then
+  mkdir "data"
+fi
+
 tesse=$1 ; exposures=$2 # e.g.: tesse: SV341N74
 telnite="${exposures:0:8}" # todo: change this, may be too custom, better to parse '_'. e.g.: 02a58884
 tessetelnite="${tesse}_${telnite}" # e.g.: SV341N74_02a58884
@@ -26,10 +31,10 @@ tol=$(echo "$3" | awk '{printf"%s\n",($1/3600.)""}') # ttt is the tolerance in d
 istesse=(${exposures})
 
 if [ -d ${tessetelnite} ]; then      # If a directory named tessetelnite exists remove it and make it clean again
-  /bin/rm -r "${tessetelnite}"
-  mkdir "${tessetelnite}"
+  /bin/rm -r "data/${tessetelnite}"
+  mkdir "data/${tessetelnite}"
 else
-  mkdir "${tessetelnite}"
+  mkdir "data/${tessetelnite}"
 fi
 # Prepares a file with the name of the tesselation and the associated exposures
 #echo "${tesse} ${istesse[@]}" > "${tessetelnite}/${tessetelnite}.exposures" # e.g.: SV341N74_02a58884
@@ -38,7 +43,7 @@ for (( i = 0 ; i < (${#istesse[@]}); i++ )); do # do this for every argument but
   exp="${istesse[i]}"
   ddcfile="/data/atlas-local/diff/${exp:0:3}/${exp:3:5}/${exp}.ddc" #; echo "${ddcfile}" # second argument is how many characters to take
   
-# Wait for the creation of the ddc file
+  # Wait for the creation of the ddc file
   out "Waiting for the file ${ddcfile} to be created."
   wait_for_file ${ddcfile} "86400"
   
@@ -61,10 +66,10 @@ done
 
 # Following line is key: jointly scans all the .ddc files of the tessellation taking only detections that a priori have more than 1% chance of being interesting to Alerce. Keeps the .ddc record, but adds
 # the exposure name and order of appearance in the .ddc file for later use in the candid name. Sorts the detections using declination.
-awk '$1~"#"{n0=NR};$1!~"#"&&$14<500&&$15<500&&$16<500&&$17<500&&$18<500&&$19<500&&$21>=0{print $0,substr(FILENAME,34,14),NR-n0}' ${awkword} > "${tessetelnite}/${tesse}_candids.tmp"
+awk '$1~"#"{n0=NR};$1!~"#"&&$14<500&&$15<500&&$16<500&&$17<500&&$18<500&&$19<500&&$21>=0{print $0,substr(FILENAME,34,14),NR-n0}' ${awkword} > "data/${tessetelnite}/${tesse}_candids.tmp"
 # Following line is key as well: Process the file with the filtered and sorted detections and finds those that coincide within ${tol}. Assign an object number and gives a unique name to each group
 # Uses Tonry's column merge program "cm" in friends of friends mode.
-/atlas/bin/cm 2,1 "${tessetelnite}/${tesse}_candids.tmp" -tol ${tol},d -grp -grporder | awk -v fids="${fids[*]}" -v obj="${tessetelnite}" -v timeobs="${timeobs[*]}" -v exposures="${istesse[*]}" ' \
+/atlas/bin/cm 2,1 "data/${tessetelnite}/${tesse}_candids.tmp" -tol ${tol},d -grp -grporder | awk -v fids="${fids[*]}" -v obj="${tessetelnite}" -v timeobs="${timeobs[*]}" -v exposures="${istesse[*]}" ' \
 function f(ra,dec) {
 
     # 19 Digit ID - two spare at the end for up to 100 duplicates
@@ -123,9 +128,9 @@ BEGIN { split(timeobs,times," "); \
 		} \
  } \
  {oid = f($2,$3);printf" %9.5f %9.5f %7.3f %6.3f %8.2f %8.2f %6.2f %6.2f %6.2f %2d %6.2f %3d %3d %3d %3d %3d %3d %3d %3d %3d %2d %9.1f %6.1f %s_%06g %s %12.6f %s\n", \
-          $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,oid,values[$25],fid[$25]}' > "${tessetelnite}/${tessetelnite}.objemjd"
+          $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,oid,values[$25],fid[$25]}' > "data/${tessetelnite}/${tessetelnite}.objemjd"
 
-/bin/rm "${tessetelnite}/${tesse}_candids.tmp" # Delete temporary file
+/bin/rm "data/${tessetelnite}/${tesse}_candids.tmp" # Delete temporary file
 # here tessetelnite is ready
 out "Calling candids for ${tessetelnite} ${exposures} "
 ./candids.sh ${tessetelnite} "${exposures}"
